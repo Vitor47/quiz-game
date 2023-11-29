0f399c44cace9467f97cf12fbfc6c55045ad83a5
Vitor Miolo <vitor.miolo@gestaods.com.br>
2023-11-28 22:18:13 -0300

    Wip: app

--- src/pages/Start/styles.js
+++ src/pages/Start/styles.js
@@ -1,9 +1,9 @@
 import styled from "styled-components/native";
-import { Text, View, SafeAreaView, ScrollView } from "react-native";
+import { SafeAreaView, ScrollView } from "react-native";
 
 export const Container = styled(SafeAreaView)`
   flex: 1;
-  background-color: #2094FF;
+  background-color: #9F7FFF;
   padding-bottom: 12%;
 `;
 
@@ -11,8 +11,19 @@ export const ScrollViewContent = styled(ScrollView)`
   padding: 20px;
 `;
 
-
 export const HeaderImg = styled.Image`
   width: 100%;
   height: 250px;
-` 
+`;
+
+export const IconPass = styled.TouchableOpacity`
+  position: fixed;
+  bottom: 47px;
+  left: 38%;
+  width: 70px;
+  padding: 20px;
+  flex-shrink: 0;
+  border-radius: 36px;
+  border: 1px solid rgba(255, 255, 255, 0.20);
+  background: #FF9051;
+`;
