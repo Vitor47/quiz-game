627f4c05cb3b4b0f1b44ed593d3dfb080956afca
TimoteoMalves <timoteo.alves14@gmail.com>
2023-12-04 21:14:28 -0300

    feature(quiz): button-component

--- src/pages/EndGame/index.jsx
+++ src/pages/EndGame/index.jsx
@@ -10,10 +10,10 @@ import {
 } from "./styles";
 import Check from "../../../assets/images/Check.png";
 import Stars3 from "../../../assets/images/Star3.png";
+import { QuestionButton } from "../../components/QuestionButton";
 import { useNavigation } from "@react-navigation/native";
 
 export default function EndGame() {
-  console.log("Endgame - result");
   return (
     <Container>
       <ScrollViewContent>
@@ -23,6 +23,7 @@ export default function EndGame() {
         <CheckImg source={Check} />
         <StarImg source={Stars3} />
         <ResultMessage>You earned X points</ResultMessage>
+        <QuestionButton></QuestionButton>
       </ScrollViewContent>
     </Container>
   );
