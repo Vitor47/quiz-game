aafef3e361614824cc7873ef8d9253be732eb305
TimoteoMalves <timoteo.alves14@gmail.com>
2023-12-05 20:33:13 -0300

    Feat: end game

--- src/pages/GameScreen/index.jsx
+++ src/pages/GameScreen/index.jsx
@@ -62,7 +62,6 @@ export default function GameScreen() {
       navigation.navigate("EndGame", {
         correctAnswers: correctAnswers,
         incorrectAnswers: incorrectAnswers,
-        numberOfQuestions: numberOfQuestions,
       });
     }
   };
